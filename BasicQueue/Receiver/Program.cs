310c57bc1a3b8f36042a6367253f8550436410bd
imfanchen <fan.chen@live.com>
2025-06-14 13:49:20 -0400

    Add Basic Queue

---
+++ BasicQueue/Receiver/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+using System.Text.Json;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+const string queueName = "basic-queue";
+
+ConnectionFactory factory = new();
+using IConnection connection = await factory.CreateConnectionAsync();
+using IChannel channel = await connection.CreateChannelAsync();
+
+await channel.QueueDeclareAsync(queue: queueName, durable: true, exclusive: false, autoDelete: false, arguments: null);
+
+Console.WriteLine(" [*] Waiting for messages.");
+
+AsyncEventingBasicConsumer consumer = new(channel);
+consumer.ReceivedAsync += (model, eventArguments) =>
+{
+    byte[] body = eventArguments.Body.ToArray();
+    string message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($" [x] Received {message}");
+    return Task.CompletedTask;
+};
+
+await channel.BasicConsumeAsync(queue: queueName, autoAck: true, consumer: consumer);
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
---
+++ BasicQueue/Sender/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Text;
+using System.Text.Json;
+using RabbitMQ.Client;
+
+const string queueName = "basic-queue";
+
+ConnectionFactory factory = new();
+using IConnection connection = await factory.CreateConnectionAsync();
+using IChannel channel = await connection.CreateChannelAsync();
+
+await channel.QueueDeclareAsync(queue: queueName, durable: true, exclusive: false, autoDelete: false, arguments: null);
+
+var messageObject = new
+{
+    Sender = "Fan Chen",
+    Model = "o4-mini",
+    Text = "What is the benefit of using RabbitMQ?"
+};
+JsonSerializerOptions serializeOptions = new()
+{
+    PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+    WriteIndented = true
+};
+string message = JsonSerializer.Serialize(messageObject, serializeOptions);
+byte[] body = Encoding.UTF8.GetBytes(message);
+
+await channel.BasicPublishAsync(exchange: string.Empty, routingKey: queueName, body: body);
+Console.WriteLine($"[X] Sent {message}");
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
