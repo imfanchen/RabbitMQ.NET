8e27ebd7c04d0b01ee57054ab5cce44a5ebd770f
imfanchen <fan.chen@live.com>
2025-06-14 13:50:04 -0400

    Add Work Queues

---
+++ WorkQueues/Manager/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+ConnectionFactory factory = new();
+using IConnection connection = await factory.CreateConnectionAsync();
+using IChannel channel = await connection.CreateChannelAsync();
+
+await channel.QueueDeclareAsync(queue: "task-queue", durable: true, exclusive: false, autoDelete: false, arguments: null);
+
+string message = GetMessage(args);
+byte[] body = Encoding.UTF8.GetBytes(message);
+
+BasicProperties properties = new() { Persistent = true };
+
+await channel.BasicPublishAsync(exchange: string.Empty, routingKey: "task-queue", mandatory: true, basicProperties: properties, body: body);
+Console.WriteLine($" [x] Sent {message}");
+
+static string GetMessage(string[] args)
+{
+    return (args.Length > 0) ? string.Join(" ", args) : "Hello World!";
+}
---
+++ WorkQueues/Worker/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+ConnectionFactory factory = new();
+using IConnection connection = await factory.CreateConnectionAsync();
+using IChannel channel = await connection.CreateChannelAsync();
+
+await channel.QueueDeclareAsync(queue: "task-queue", durable: true, exclusive: false, autoDelete: false, arguments: null);
+
+await channel.BasicQosAsync(prefetchSize: 0, prefetchCount: 1, global: false);
+
+Console.WriteLine(" [*] Waiting for messages.");
+
+AsyncEventingBasicConsumer consumer = new(channel);
+consumer.ReceivedAsync += async (ModuleHandle, eventArguments) =>
+{
+    byte[] body = eventArguments.Body.ToArray();
+    string message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($" [x] Received {message}");
+
+    int dots = message.Count(c => c == '.');
+    Console.WriteLine($" [x] Processing...");
+    await Task.Delay(dots * 1000);
+    Console.WriteLine($" [x] Done!");
+
+    await channel.BasicAckAsync(deliveryTag: eventArguments.DeliveryTag, multiple: false);
+};
+
+await channel.BasicConsumeAsync(queue: "task-queue", autoAck: false, consumer: consumer);
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
