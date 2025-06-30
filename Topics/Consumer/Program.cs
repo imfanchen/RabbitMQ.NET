1a47576e0e235445a6e3117e3a1dd66d78e2f555
imfanchen <fan.chen@live.com>
2025-06-30 17:29:30 -0400

    Add Topics

---
+++ Topics/Consumer/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+const string exchangeName = "crypto-prices";
+
+if (args.Length < 1)
+{
+    Console.Error.WriteLine("Usage: dotnet run [<exchange>.<symbol>]...");
+    Console.WriteLine(" Press [enter] to exit.");
+    Console.ReadLine();
+    Environment.ExitCode = 1;
+    return;
+}
+
+ConnectionFactory factory = new();
+using IConnection connection = await factory.CreateConnectionAsync();
+using IChannel channel = await connection.CreateChannelAsync();
+
+await channel.ExchangeDeclareAsync(exchange: exchangeName, type: ExchangeType.Topic);
+
+QueueDeclareOk queueInstance = await channel.QueueDeclareAsync();
+string queueName = queueInstance.QueueName;
+
+foreach (string bindingKey in args)
+{
+    await channel.QueueBindAsync(queue: queueName, exchange: exchangeName, routingKey: bindingKey);
+}
+
+Console.WriteLine(" [*] Waiting for messages.");
+
+AsyncEventingBasicConsumer consumer = new(channel);
+consumer.ReceivedAsync += (model, eventArguments) =>
+{
+    string routingKey = eventArguments.RoutingKey;
+    byte[] body = eventArguments.Body.ToArray();
+    string message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($" [x] Received '{routingKey}': '{message}'");
+    return Task.CompletedTask;
+};
+
+await channel.BasicConsumeAsync(queue: queueName, consumer: consumer, autoAck: true);
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
---
+++ Topics/Producer/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+const string exchangeName = "crypto-prices";
+
+ConnectionFactory factory = new();
+using IConnection connection = await factory.CreateConnectionAsync();
+using IChannel channel = await connection.CreateChannelAsync();
+
+await channel.ExchangeDeclareAsync(exchange: exchangeName, type: ExchangeType.Topic);
+
+string routingKey = (args.Length > 0) ? args[0] : "coinbase.btc-usd";
+
+// In real world scenarios, the prices should be fetched from exchange's api
+string message = (args.Length > 1) ? args[1] : "100,000";
+byte[] body = Encoding.UTF8.GetBytes(message);
+await channel.BasicPublishAsync(exchange: exchangeName, routingKey: routingKey, body: body);
+Console.WriteLine($" [x] Sent '{routingKey}':'{message}'");
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
