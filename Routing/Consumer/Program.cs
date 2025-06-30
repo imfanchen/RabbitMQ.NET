a426d883758db399d57e0fe6f3f034e31a3050d9
imfanchen <fan.chen@live.com>
2025-06-30 17:28:06 -0400

    Add Routing

---
+++ Routing/Consumer/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+if (args.Length < 1)
+{
+    Console.Error.WriteLine("Usage: dotnet run [debug] [info] [warning] [error]");
+    Console.WriteLine(" Press [enter] to exit.");
+    Console.ReadLine();
+    Environment.ExitCode = 1;
+    return;
+}
+
+const string exchangeName = "routing-logs";
+
+ConnectionFactory factory = new();
+IConnection connection = await factory.CreateConnectionAsync();
+IChannel channel = await connection.CreateChannelAsync();
+
+await channel.ExchangeDeclareAsync(exchange: exchangeName, type: ExchangeType.Direct);
+
+QueueDeclareOk queueInstance = await channel.QueueDeclareAsync();
+string queueName = queueInstance.QueueName;
+
+foreach (string severity in args)
+{
+    await channel.QueueBindAsync(queue: queueName, exchange: exchangeName, routingKey: severity);
+}
+
+Console.WriteLine(" [*] Waiting for logs.");
+
+AsyncEventingBasicConsumer consumer = new(channel);
+consumer.ReceivedAsync += (model, exchangeArguments) =>
+{
+    string routingKey = exchangeArguments.RoutingKey;
+    byte[] body = exchangeArguments.Body.ToArray();
+    string message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($" [x] Received {routingKey}: {message}");
+    return Task.CompletedTask;
+};
+
+await channel.BasicConsumeAsync(queue: queueName, autoAck: true, consumer: consumer);
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
---
+++ Routing/Producer/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+string[] allowedLevels = { "debug", "info", "warning", "error" };
+if (args.Length == 0 || !allowedLevels.Contains(args[0], StringComparer.OrdinalIgnoreCase))
+{
+    Console.WriteLine("Invalid or missing log level. Please use one of: debug, info, warning, error.");
+    Console.WriteLine(" Press [enter] to exit.");
+    Console.ReadLine();
+    Environment.ExitCode = 1;
+    return;
+}
+
+const string exchangeName = "routing-logs";
+
+ConnectionFactory factory = new();
+using IConnection connection = await factory.CreateConnectionAsync();
+using IChannel channel = await connection.CreateChannelAsync();
+
+await channel.ExchangeDeclareAsync(exchange: exchangeName, type: ExchangeType.Direct);
+
+string severity = args.Length > 0 ? args[0] : "debug";
+string message = args.Length > 1 ? string.Join(" ", args.Skip(1).ToArray()) : "Hello World!";
+byte[] body = Encoding.UTF8.GetBytes(message);
+await channel.BasicPublishAsync(exchange: exchangeName, routingKey: severity, body: body);
+Console.WriteLine($" [x] Sent '{severity}':'{message}'");
+
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
