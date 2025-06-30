477e22816034fd4d7f1d83381fc9b3774171f851
imfanchen <fan.chen@live.com>
2025-06-30 17:27:33 -0400

    Add Publish Subscribe

---
+++ PublishSubscribe/Consumer/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+const string exchangeName = "pub-sub-logs";
+
+ConnectionFactory factory = new();
+using IConnection connection = await factory.CreateConnectionAsync();
+using IChannel channel = await connection.CreateChannelAsync();
+
+await channel.ExchangeDeclareAsync(exchange: exchangeName, type: ExchangeType.Fanout);
+
+QueueDeclareOk queueInstance = await channel.QueueDeclareAsync();
+string queueName = queueInstance.QueueName;
+
+await channel.QueueBindAsync(queue: queueName, exchange: exchangeName, routingKey: string.Empty);
+
+Console.WriteLine(" [*] Waiting for logs.");
+
+AsyncEventingBasicConsumer consumer = new(channel);
+consumer.ReceivedAsync += (model, eventArguments) =>
+{
+    byte[] body = eventArguments.Body.ToArray();
+    string message = Encoding.UTF8.GetString(body);
+    Console.WriteLine($" [x] Received {message}");
+    return Task.CompletedTask;
+};
+
+await channel.BasicConsumeAsync(queue: queueName, autoAck: true, consumer: consumer);
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
---
+++ PublishSubscribe/Producer/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+const string exchangeName = "pub-sub-logs";
+
+ConnectionFactory factory = new();
+using IConnection connection = await factory.CreateConnectionAsync();
+using IChannel channel = await connection.CreateChannelAsync();
+
+await channel.ExchangeDeclareAsync(exchange: exchangeName, type: ExchangeType.Fanout);
+
+Func<string[], string> getMessage = args => args.Length > 0 ? string.Join(" ", args) : "Hello, World!";
+string message = getMessage(args);
+
+byte[] body = Encoding.UTF8.GetBytes(message);
+await channel.BasicPublishAsync(exchange: exchangeName, routingKey: string.Empty, body: body);
+
+Console.WriteLine($" [x] Sent {message}");
+
+Console.WriteLine(" Press [enter] to exit.");
+Console.ReadLine();
